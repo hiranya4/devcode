1aaedf716b951ca58428f81044ae947897888dfc
hiranya4 <hiranyak.patra121@gmail.com>
2025-12-29 00:08:11 +0530

    Update testj.java

--- testj.java
+++ testj.java
@@ -1,5 +1,5 @@
-public class Main {
-public static void main(String[] args) {
-System.out.println("Hello World");
-}
+public class testj {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
